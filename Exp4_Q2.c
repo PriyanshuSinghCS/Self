18b90441ddc9ec6f3d2880106a71876fd248bc30
Priyanshugitup <priyanshukk2025@gmail.com>
2025-11-07 19:34:34 +0530

    Experiment4

---
+++ Exp4_Q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int GCD(int num1, int num2) {
+    if (num2 == 0)
+        return num1;
+    else
+        return GCD(num2, num1 % num2);
+}
+
+int main() {
+    int a, b, gcd;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+
+    gcd = GCD(a, b);
+
+    printf("The GCD of %d and %d is: %d\n", a, b, gcd);
+
+    return 0;
+}
