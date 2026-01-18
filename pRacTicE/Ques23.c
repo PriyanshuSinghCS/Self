820449ef423357357b9810e0ccf5555ebb903493
Priyanshugitup <priyanshukk2025@gmail.com>
2026-01-18 13:24:01 +0530

    jan 18

---
+++ pRacTicE/Ques23.c
@@ -0,0 +1,35 @@
+// Find the transpose of a matrix.
+
+#include <stdio.h>
+
+int main(){
+    int r;
+    printf("Enter no of rows: ");
+    scanf("%d",&r);
+    int c;
+    printf("Enter no of columns: ");
+    scanf("%d",&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     
+    printf("Orriginal Matrix: \n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("Transpose of the matrix:\n");
+    for(int i=0;i<c;i++){
+        for(int j=0;j<r;j++){
+            printf("%d ",arr[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
